40142c487dc54f33dea039b99ae81f3c6b0c3cd1
vikash singh <singhvikash301094@gmail.com>
2018-01-11 14:52:14 +0530

    testing the Hashmap in generics

--- Generics.java
+++ Generics.java
@@ -20,7 +20,7 @@ public class Generics {
 
              studentNewMap  =   studentNewMap.put("Xyz","singh");
 
-             //replacing the existing key value in hashmap
+             //replacing the existing key value in studentNewMap
              studentNewMap  =   studentNewMap.put("vikash","kumar");
 
             Iterator studentMapIterator = studentMap.iterator();
@@ -46,6 +46,35 @@ public class Generics {
                 System.out.println(temp.getKey() + " " + temp.getValue());
 
             }
+      /*
+            //removing the entry in studentNewMap
+            studentNewMap = studentNewMap.remove("vikash");
+
+            studentNewMapIterator = studentNewMap.iterator();
+
+            System.out.println("\nprinting all value of the studentNewMap after removing\n");
+
+
+            while(studentNewMapIterator.hasNext())
+            {
+                //remember type casting
+                Entry<String,String> temp = (Entry)studentNewMapIterator.next();
+                System.out.println(temp.getKey() + " " + temp.getValue());
+
+            }
+
+            studentMapIterator = studentMap.iterator();
+
+            System.out.println("printing all value of the studentMap\n");
+
+            while(studentMapIterator.hasNext())
+            {
+                //remember type casting
+                Entry<String,String> temp = (Entry)studentMapIterator.next();
+                System.out.println(temp.getKey() + " " + temp.getValue());
+
+            }
+    */
 
         }
         catch (Exception e)
