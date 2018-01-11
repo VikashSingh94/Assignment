3c1332d3f4138311b2553db6ef89f55b13292105
vikash singh <singhvikash301094@gmail.com>
2018-01-11 12:33:18 +0530

    testing the HashMap

--- Generics.java
+++ Generics.java
@@ -4,21 +4,52 @@ import java.util.*;
 
 public class Generics {
 
+
     public static void main(String args[])throws Exception
     {
-        HashMap<String,String> a= new HashMap<>();
-        HashMap<String,String> b= new HashMap<>();
 
-        try {
-             a =  a.add("vikash", "Singh");
-              a.add("Vickey", "Singh");
 
-            System.out.println(a.get("vikash"));
+        HashMap<String,String> studentMap   =   new  HashMap<>();
+        HashMap<String,String> studentNewMap;
+
+        try
+        {
+             studentMap =  studentMap.put("vikash", "Singh");
+
+             studentNewMap =  studentMap.put("Vickey", "Singh");
+
+             studentNewMap =  studentNewMap.put("Xyz","singh");
+
+            Iterator studentMapIterator = studentMap.iterator();
+
+            Iterator studentNewMapIterator = studentNewMap.iterator();
+
+            System.out.println("printing all value of the studentMap\n");
+
+            while(studentMapIterator.hasNext())
+            {
+                //remember type casting
+                Entry<String,String> temp = (Entry)studentMapIterator.next();
+                System.out.println(temp.getKey() + " " + temp.getValue());
+
+            }
+
+            System.out.println("\nprinting all value of the studentNewMap\n");
+
+            while(studentNewMapIterator.hasNext())
+            {
+                //remember type casting
+                Entry<String,String> temp = (Entry)studentNewMapIterator.next();
+                System.out.println(temp.getKey() + " " + temp.getValue());
+
+            }
+
         }
         catch (Exception e)
         {
             System.out.println(e);
         }
+
 
     }
 }
